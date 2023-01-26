a4f79b7f61ee7c986f808344ae1885fb38597131
Joeri Malmberg <joeri.malmberg@blackbird.cloud>
2023-01-26 12:13:10 +0100

    Updates

--- main.tf
+++ main.tf
@@ -1,8 +1,28 @@
 module "deployment" {
   source  = "blackbird-cloud/deployment/helm"
-  version = "0.0.4"
+  version = "1.0.1"
 
-  values    = var.values
-  name      = var.name
-  namespace = var.namespace
+  name        = var.name
+  description = var.description
+  namespace   = var.namespace
+
+  repository    = var.repository
+  chart         = var.chart
+  chart_version = var.chart_version
+
+  create_namespace = var.create_namespace
+  disable_webhooks = var.disable_webhooks
+
+  repository_username = var.repository_username
+  repository_password = var.repository_password
+
+  values = concat(var.values, var.sensitive_values)
+
+  cleanup_on_fail = var.cleanup_on_fail
+  force_update    = var.force_update
+  timeout         = var.timeout
+  recreate_pods   = var.recreate_pods
+  atomic          = var.atomic
+  wait            = var.wait
+  wait_for_jobs   = var.wait_for_jobs
 }
